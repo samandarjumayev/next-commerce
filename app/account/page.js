11e3ecb3b5628224b73d80feb1fe85053554d35c
BLACKxOw1 <nuriddintuychiyev5@gmail.com>
2025-12-06 17:44:59 +0500

    salom

--- app/account/page.js
+++ app/account/page.js
@@ -1,8 +1,14 @@
-
+import { Slash } from 'lucide-react';
 
 export default function Account(){
   return <div>
-    Account
+    <div>
+      <div className='flex items-center gap-3'>
+        <p className='text-[#000000]'>Home</p>
+        <p>/</p>
+        <p>My Account</p>
+      </div>
+    </div>
   </div>
 }
 
