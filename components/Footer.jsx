c50d61c9e680b4ad50f11433177a7e63e0984731
samandarjumayev <samandarjumayev005@gmail.com>
2025-12-06 17:44:28 +0500

    yangilandi, lucide-react bor

---
+++ components/Footer.jsx
@@ -0,0 +1,6 @@
+
+export default function Footer(){
+    return <div className="bg-black text-cyan-500 text-center py-10 w-full">
+        Footer | NajotTalim
+    </div>
+}
---
+++ components/Header.jsx
@@ -0,0 +1,6 @@
+
+export default function Header(){
+    return <div className="w-full h-[70px] flex items-center justify-between">
+
+    </div>
+}
