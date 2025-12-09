7a14554ad5946d4dd206e96aeae58fbe4cb7da58
samandarjumayev <samandarjumayev005@gmail.com>
2025-12-09 14:48:40 +0500

    update

--- components/CategoryComponent.jsx
+++ components/CategoryComponent.jsx
@@ -3,16 +3,28 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function CategoryComponent(){
-    const res = await fetch('https://dummyjson.com/products', {
-        cache: 'no-store'
-    });
-    const pos = await fetch('https://fakestoreapi.com/products', {
-        cache: 'no-store'
-    })
-    const products = await res.json();
-    const products2 = await pos.json();
+    let products = { products: [] };
+    let products2 = [];
 
-    const categories = [...new Set(products.products.map(item => item.category)), ...new Set(products2.map(item => item.category))]
+    try {
+    const res = await fetch('https://dummyjson.com/products', { cache: 'no-store' });
+    products = await res.json();
+    } catch (err) {
+    console.error('DummyJSON fetch error:', err);
+    }
+
+    try {
+    const pos = await fetch('https://fakestoreapi.com/products', { cache: 'no-store' });
+    products2 = await pos.json();
+    } catch (err) {
+    console.error('FakeStore fetch error:', err);
+    }
+
+    const categories = [
+        ...new Set(products?.products?.map(item => item.category)), 
+        ...new Set(products2?.map(item => item.category))
+    ]
+    
     return <div className="w-full flex gap-5 mb-10">
         <div className="border-r border-zinc-300 py-3 pr-5 flex flex-col gap-2">
             {categories.map(item => {
